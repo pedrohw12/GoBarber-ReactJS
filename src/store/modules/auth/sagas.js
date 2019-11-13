3a525dac8b8b57e7b8db6d0c27e5f09119484b99
Pedro Wanderley <peu-zico>
2019-11-13 06:04:43 -0300

    Login authentication

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -3,10 +3,11 @@ import { takeLatest, call, put, all } from 'redux-saga/effects';
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
   const response = yield call(api.post, 'sessions', {
     email,
@@ -23,6 +24,9 @@ export function* signIn({ payload }) {
   yield put(signInSuccess(token, user));
 
   history.push('/dashboard');
+  } catch(err) {
+    yield put(signFailure());
+  }
 }
 
 export default all([
